b5c43db31ba374c1058c91fc7854ca2e32a8ab78
Deivao1337 <contatodeivao@gmail.com>
2024-05-09 20:00:41 -0300

    1.3

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
+  var portifolioDiv = document.getElementById("portifolio");
   var desaparecerDiv = document.getElementById("desaparecer");
   var aparecer = document.getElementById("aparecer");
   var voltarDiv = document.getElementById("voltar");
 
-  desaparecerDiv.addEventListener("click", function () {
+  portifolioDiv.addEventListener("click", function () {
     desaparecerDiv.style.display = "none";
     aparecer.style.display = "block";
   });
