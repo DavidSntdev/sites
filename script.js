622ba24d8b00a872a37d965ae74a6cd91af98dc3
Deivao1337 <contatodeivao@gmail.com>
2024-05-09 16:30:55 -0300

    1.2

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   var desaparecerDiv = document.getElementById("desaparecer");
   var aparecer = document.getElementById("aparecer");
+  var voltarDiv = document.getElementById("voltar");
 
   desaparecerDiv.addEventListener("click", function () {
     desaparecerDiv.style.display = "none";
     aparecer.style.display = "block";
+  });
+  voltarDiv.addEventListener("click", function () {
+    desaparecerDiv.style.display = "block";
+    aparecer.style.display = "none";
   });
 });
